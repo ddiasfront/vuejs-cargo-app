0aadd8ca87925b31911006317345879a4d6d5f67
Dias <d.dias@accenture.com>
2019-03-28 18:27:14 -0300

    first commit

---
+++ src/app/Main.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div class="col-12 col-md-8 mainbg">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+  components: {
+    // 'header-component': Header,
+    // 'title-component': Title,
+    // 'techs': Techs,
+    // 'footer-component': Footer
+  }
+};
+</script>
