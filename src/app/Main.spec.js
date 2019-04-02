9ff12a45208d4f8287e954a1a47af7a18124d3c2
Dias <d.dias@accenture.com>
2019-04-02 16:17:20 -0300

    tests linting

--- src/app/Main.spec.js
+++ src/app/Main.spec.js
@@ -1,11 +1,22 @@
 import Main from './Main.vue';
-
+import moxios from 'moxios';
+// import axios from 'axios';
 describe('Main', () => {
+  beforeEach(() => {
+    // import and pass your custom axios instance to this method
+    moxios.install();
+  });
+
+  afterEach(() => {
+    // import and pass your custom axios instance to this method
+    moxios.uninstall();
+  });
+
   it('should be a main', () => {
     expect(Main.name).toEqual('Main');
   });
-  it('gets data from api', () => {
-  });
+  // it('gets data from api', () => {
+  // });
   // it('formato de data deve ser igual modelo api ', () => {
   //   // MOCKAR API
   //   // DEFINIR VARIAVEL BOXARRAY DEVE SER IGUAL A MOCK.
